e293133086e85aeb6cce4e708ea5d2ff7cc8d359
Anagesh <amachira@hawk.iit.edu>
2020-04-14 03:46:53 +0000

    updated login

---
+++ src/login.php
@@ -0,0 +1,39 @@
+<?php  session_start();   // session starts with the help of this function 
+?>
+
+<?php
+
+
+
+if(isset($_POST['Submit']))   // it checks whether the user clicked login button or not 
+{
+
+if (empty($_POST['username']) || empty($_POST['password'])) {
+
+header('location:index.php?msg=Please enter some username and password');
+
+$error = "Username or Password is invalid";
+echo "Enter the Username and password";
+}
+else
+{
+     $username = $_POST['username'];
+     $password = $_POST['password'];
+
+      if($username == "anagesh" && $password == "1234")  
+       {	  
+		          
+        //echo '<script type="text/javascript"> window.open("welcome.php","_self");</script>';         
+		header('/welcome.php');
+        }
+
+        else
+        {
+            echo "invalid UserName or Password";    
+			header('location:index.php?msg=Please enter some username and password');			
+        }
+}
+}
+ ?>
+
+
